932220f59df72071dcbcc84e3cbd3ac6fde7601d
jordano96 <byron-jordan@hotmail.com>
2020-10-07 21:26:01 -0500

    Login y registro terminado

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,82 @@
+import 'package:chat/widgets/boton_azul.dart';
+import 'package:chat/widgets/custom_input.dart';
+import 'package:chat/widgets/labels.dart';
+import 'package:chat/widgets/logo.dart';
+import 'package:flutter/material.dart';
+
+
+class RegisterPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              height: MediaQuery.of(context).size.height*0.90,
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Logo(titulo: 'Registro',),
+                _Form(),
+                Labels(
+                  ruta: 'login',
+                  titulo: 'Ya tienes una cuenta',
+                  subtitulo: 'Ingresa ahora!',
+                  ),
+                Text('Terminos y condiciones de uso',style: TextStyle(fontWeight: FontWeight.w200),)
+              ],
+        ),
+            ),
+          ),
+      )
+   );
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final nnameCtrl=TextEditingController();
+  final emailCtrl=TextEditingController();
+  final passCtrl=TextEditingController();
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          CustomInput(
+            icon: Icons.perm_identity,
+            placeholder: 'Nombre',
+            keyboardType: TextInputType.text,
+            textController: nnameCtrl,
+          ),
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contrasena',
+            textController: passCtrl,
+            isPassword: true,
+          ),
+          BotonAzul(text: 'Registrar',onPressed: (){},)
+        ],
+
+      ),
+      
+    );
+  }
+}
